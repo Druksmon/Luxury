89b6567ca0d2c606ec9ddb4132729c6b90c626e3
Dario Volkmann <62895783+Druksmon@users.noreply.github.com>
2022-09-27 17:46:56 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
+$(document).ready(function () {
+    $(window).scroll(function () {
         if ($(document).scrollTop() > 2500) {
             $("#footer").addClass("show");
         } else {
             $("#footer").removeClass("show");
         }
     });
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 100) {
+            $(".back_top").css({opacity: 1});
+            $('.nav_container').addClass("nav_container-background");
+        } else {
+            $(".back_top").css({opacity: 0});
+            $('.nav_container').removeClass("nav_container-background");
+        }
+    });
+});
+
+
+// NAV
+
+
+$('.nav_toggle').on('click', function (e) {
+
+    $('.nav_links').toggleClass("nav_links-visible");
+    $('.burger').toggleClass("burger_black");
+
 });
